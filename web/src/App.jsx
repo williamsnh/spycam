10839d46464bb8905e9a602108b5b57e5863eb93
William Singh <william.singh027@gmail.com>
2026-07-08 20:38:27 +0200

    Commit 8: Communication dynamique entre l'interface et le server.py

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+  const wsRef = useRef(null);
+
   useEffect(() => {
-  const ws = new WebSocket("ws://127.0.0.1:8000/ws");
+    const ws = new WebSocket("ws://127.0.0.1:8000/ws");
+    ws.onopen = () => console.log("connecté au robot");
+    ws.onclose = () => console.log("déconnecté");
+    wsRef.current = ws;
 
-  ws.onopen = () => console.log("connecté au robot");
-  ws.onclose = () => console.log("déconnecté");
-
-  return () => ws.close();
+    return () => ws.close();
   }, []);
+
+  const sendForward = () => {
+    wsRef.current.send("forward");
+  };
+
   return (
     <div>
-      <h1>Spycam</h1>
-      <p>Interface de supervision à venir</p>
-        <img src="http://127.0.0.1:8000/stream" alt="flux vidéo du robot" />
+      <h1>spycam</h1>
+      <img src="http://127.0.0.1:8000/stream" alt="flux vidéo du robot" />
+      <button onClick={sendForward}>Avancer</button>
     </div>
   );
 }
 
-export default App
+export default App;
